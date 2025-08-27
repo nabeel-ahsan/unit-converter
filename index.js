ce77a2cbaa537c304bbda262d3d0694062e6ce09
nabeel-ahsan <itsme007nabeel@gmail.com>
2025-08-28 00:42:55 +0530

    update index.html and index.css

--- index.js
+++ index.js
@@ -3,3 +3,7 @@
 1 liter = 0.264 gallon
 1 kilogram = 2.204 pound
 */
+
+const lengthEl = document.getElementById("length")
+const volumeEl = document.getElementById("volume")
+const massEl = document.getElementById("mass")
